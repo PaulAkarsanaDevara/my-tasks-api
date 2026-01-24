0d5c8683bc3bf5eef0da2c3d572628b870bf4a34
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2026-01-24 20:43:02 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import compression from 'compression';
 
 import { errorHandler } from './middlewares/errorHandler';
 
@@ -8,5 +9,6 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(compression());
 
 app.use(errorHandler);
